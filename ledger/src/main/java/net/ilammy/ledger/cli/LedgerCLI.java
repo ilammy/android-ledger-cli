4d5f238fa195812aa21617f1a1ae007525508173
ilammy <me@ilammy.net>
2020-06-23 21:40:02 +0300

    Improved Java interface
    Add some more methods and make the Java wrapping interface actually
    useful. We need access to Android app context to be able to run Ledger
    binary from the app's library directory.

--- ledger/src/main/java/net/ilammy/ledger/cli/LedgerCLI.java
+++ ledger/src/main/java/net/ilammy/ledger/cli/LedgerCLI.java
@@ -1,18 +1,70 @@
 package net.ilammy.ledger.cli;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+/**
+ * Command-line interface to Ledger.
+ */
 public class LedgerCLI {
-    public static Process exec(String... args) {
-        String[] command = new String[args.length + 1];
-        command[0] = "lib...hello...so";
-        System.arraycopy(args, 0, command, 1, args.length);
+    private final Context context;
 
+    /**
+     * Initialize Ledger CLI with Android context.
+     * @param context Android context to use
+     */
+    public LedgerCLI(@NonNull Context context) {
+        this.context = context;
+    }
+
+    private static final String LEDGER_PATH = "lib/lib...ledger...so";
+
+    private String getLedgerPath() {
+        return new File(context.getDataDir(), LEDGER_PATH).getAbsolutePath();
+    }
+
+    /**
+     * Execute Ledger with provided arguments.
+     * @param args command-line arguments to Ledger
+     * @return Ledger process descriptor.
+     */
+    public Process exec(@NonNull String... args) {
         try {
-            return Runtime.getRuntime().exec(command);
-        }
-        catch (IOException e) {
+            return prepare(args).start();
+        } catch (IOException e) {
+            // This exception can happen if the binary file is not executable.
+            // Our file should be executable, otherwise it's an error.
             throw new RuntimeException(e);
         }
+    }
+
+    /**
+     * Prepare a new Ledger invocation.
+     * @param args command-line arguments to Ledger
+     * @return Ledger process builder.
+     */
+    public ProcessBuilder prepare(@NonNull String... args) {
+        return prepare(Arrays.asList(args));
+    }
+
+    /**
+     * Prepare a new Ledger invocation.
+     * @param args command-line arguments to Ledger
+     * @return Ledger process builder.
+     */
+    public ProcessBuilder prepare(@NonNull List<String> args) {
+        ArrayList<String> fullArgs = new ArrayList<>();
+        fullArgs.add(getLedgerPath());
+        fullArgs.addAll(args);
+
+        ProcessBuilder builder = new ProcessBuilder();
+        builder.command(fullArgs);
+        return builder;
     }
 }
