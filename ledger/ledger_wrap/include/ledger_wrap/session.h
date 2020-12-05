66682d553740aff557294c0c2cbb56bdfb03f874
ilammy <me@ilammy.net>
2020-12-05 13:42:36 +0900

    Update session_ptr interface
    Make the session_ptr actually useful. It will look like a smart pointer,
    but actually it's a 'dumb pointer' -- the one which does not really
    manage memory. However, you can still use "->" to call methods of
    ledger::session_t. For this sake another wrapper is introduced which
    actually forwards calls.

--- ledger/ledger_wrap/include/ledger_wrap/session.h
+++ ledger/ledger_wrap/include/ledger_wrap/session.h
@@ -1,22 +1,90 @@
 #ifndef LEDGER_WRAP_SESSION_H
 #define LEDGER_WRAP_SESSION_H
 
+#include <string>
+
 #include <ledger_wrap/forward.h>
 
 namespace ledger_wrap
 {
 
-class session_ptr
+// Forward declaration for the benefit of friends.
+class session_ptr;
+
+// We have to declare this first because C++ still pretends to be single-pass.
+namespace impl
+{
+
+/// Wrapper which forwards methods of `ledger::session_t*`.
+///
+/// This one exists for the sake of `operator->` working as expected.
+class session_t
 {
     ledger::session_t *session;
 
 public:
-    static session_ptr make();
-    static void free(session_ptr &session);
+    /// Reads journal data into this session from given string.
+    ///
+    /// This invalidates all previously obtained accessors into journal data.
+    session_t& read_journal_from_string(const std::string &data);
 
-    session_ptr(ledger::session_t *session)
+private:
+    friend class ledger_wrap::session_ptr;
+
+    explicit session_t(ledger::session_t *session) noexcept
       : session(session)
     {
+    }
+};
+
+} // namespace ledger_wrap::impl
+
+/// Dumb pointer equivalent of `ledger::session_t*`
+///
+/// Note that it's 'dumb' and does not track ownership or references.
+/// You need to manage the memory manually via `make()` and `free()`.
+///
+/// You are allowed to copy and destroy this pointer as much as you want,
+/// this does not affect the object it points to in any way.
+class session_ptr
+{
+    impl::session_t session;
+
+public:
+    /// Allocates a new session and wraps it into a `session_ptr`.
+    static session_ptr make();
+
+    /// Deallocates the session pointed to by this `session_ptr`.
+    static void free(session_ptr &session);
+
+    /// Wraps a raw session pointer.
+    explicit session_ptr(ledger::session_t *session) noexcept
+      : session(session)
+    {
+    }
+
+    /// Returns the raw session pointer.
+    inline ledger::session_t* as_ptr() const noexcept
+    {
+        return session.session;
+    }
+
+    /// Checks whether the session pointer is null.
+    inline bool operator!() const noexcept
+    {
+        return !session.session;
+    }
+
+    /// Dereferences this pointer into session object.
+    inline impl::session_t* operator->() noexcept
+    {
+        return &session;
+    }
+
+    /// Dereferences this pointer into session object.
+    inline const impl::session_t* operator->() const noexcept
+    {
+        return &session;
     }
 };
 
--- ledger/ledger_wrap/src/session.cc
+++ ledger/ledger_wrap/src/session.cc
@@ -13,13 +13,24 @@ session_ptr session_ptr::make()
 {
     auto session = new ledger::session_t();
     init_ledger_globals(session);
-    return session;
+    return session_ptr(session);
 }
 
 void session_ptr::free(session_ptr &session)
 {
-    delete session.session;
-    session.session = nullptr;
+    delete session.session.session;
+    session.session.session = nullptr;
 }
+
+namespace impl
+{
+
+session_t& session_t::read_journal_from_string(const std::string &data)
+{
+    session->read_journal_from_string(data);
+    return *this;
+}
+
+} // namespace ledger_wrap::impl
 
 } // namespace ledger_wrap
